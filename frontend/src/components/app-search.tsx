ca85f0922b5a4aba6abd46541f83eb8e311862a8
mohamedhayballa <mohamedhayballa22@gmail.com>
2025-10-10 12:04:40 +0000

    Change ctrl+S to ctrl+K as it's the universal global search hotkey

--- frontend/src/components/app-search.tsx
+++ frontend/src/components/app-search.tsx
@@ -35,7 +35,7 @@ export function SearchDialog() {
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault()
         setOpen((open) => !open)
       }
@@ -69,7 +69,7 @@ export function SearchDialog() {
             {modifierKey}
           </kbd>
           <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
-            S
+            K
           </kbd>
         </div>
       </div>
