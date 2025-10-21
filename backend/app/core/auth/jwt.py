48f2fa41796b1e009143c6cee15e6a101fa0a6b0
mohamedhayballa <mohamedhayballa22@gmail.com>
2025-10-21 20:05:26 +0000

    feat(auth): Switch from Bearer to Cookie transport for JWT authentication

--- backend/app/core/auth/jwt.py
+++ backend/app/core/auth/jwt.py
@@ -1,17 +1,23 @@
-from fastapi_users.authentication import JWTStrategy, AuthenticationBackend, BearerTransport
+from fastapi_users.authentication import JWTStrategy, AuthenticationBackend, CookieTransport
 from app.core.config import settings
 
-bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+cookie_transport = CookieTransport(
+    cookie_max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+    cookie_name="access_token",
+    cookie_secure=True,
+    cookie_httponly=True,
+)
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=settings.SECRET_KEY,
         lifetime_seconds=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
-        token_audience=["nooryx"],
+        token_audience=["nooryx_users"],
     )
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=bearer_transport,
+    transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
