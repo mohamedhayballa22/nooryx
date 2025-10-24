9f68da672482fb4e1836a566c9b370644a96c68f
mohamedhayballa <mohamedhayballa22@gmail.com>
2025-10-24 18:12:46 +0000

    refactor: remove auth logic now handled by layout

--- frontend/src/app/(guest)/page.tsx
+++ frontend/src/app/(guest)/page.tsx
@@ -1,31 +1,7 @@
-"use client";
-
-import { useAuth } from "@/lib/auth";
-import { AuthLoading } from "@/components/auth-loading";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function LandingPage() {
-  const { isAuthenticated, isLoading } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isLoading && isAuthenticated) {
-      router.push("/core/dashboard");
-    }
-  }, [isLoading, isAuthenticated, router]);
-
-  if (isLoading) {
-    return <AuthLoading />;
-  }
-
-  if (isAuthenticated) {
-    return null; // Redirecting...
-  }
-
   return (
     <div className="h-200 pt-10">
-        <h1>Hey, This is the landing page (you aren't logged in)</h1>
+      <h1>Hey, This is the landing page (you aren't logged in)</h1>
     </div>
   );
 }
