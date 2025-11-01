fa0ad8543a8b53a3793d86d9f933dc7604a86555
mohamedhayballa <mohamedhayballa22@gmail.com>
2025-11-01 11:44:55 +0000

    feat: add valuation page
    Create main valuation page combining header and table components with integrated data fetching and state management

---
+++ frontend/src/app/core/valuation/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useState } from "react"
+import { ValuationHeader } from "./components/valuation-header"
+import { ValuationDataTable } from "./components/valuation-skus-table"
+import { PaginationState } from "@tanstack/react-table"
+import { useSKUValuations } from "./hooks/use-sku-valuations"
+import { useTotalValuation } from "./hooks/use-total-valuation"
+
+export default function Page() {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  })
+
+  const { data: headerData, isLoading: isLoadingHeader, refetch: refetchHeader, isRefetching: isRefetchingHeader} = useTotalValuation()
+  const { data: tableData, isLoading: isLoadingTable } = useSKUValuations(
+    pagination.pageIndex,
+    pagination.pageSize
+  )
+
+  const handleRefresh = async () => {
+    await refetchHeader()
+  }
+
+  return (
+    <div className="space-y-8">
+      {isLoadingHeader ? (
+        <ValuationHeader.Skeleton />
+      ) : (
+        headerData && (
+          <ValuationHeader 
+            {...headerData} 
+            onRefresh={handleRefresh}
+            isLoading={isRefetchingHeader}
+          />
+        )
+      )}
+      <div>
+        {isLoadingTable ? (
+          <ValuationDataTable.Skeleton />
+        ) : (
+          tableData && (
+            <ValuationDataTable
+              data={tableData.items}
+              pagination={pagination}
+              onPaginationChange={setPagination}
+              totalPages={tableData.pages}
+              totalItems={tableData.total}
+            />
+          )
+        )}
+      </div>
+    </div>
+  )
+}
