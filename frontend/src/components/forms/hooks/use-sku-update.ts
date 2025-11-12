d95e8ebe922f35454ab46b9f8a2c7efb849f4b7c
mohamedhayballa <mohamedhayballa22@gmail.com>
2025-11-12 10:57:11 +0000

    Invalidate iventory query cache after updating SKU

--- frontend/src/components/forms/hooks/use-sku-update.ts
+++ frontend/src/components/forms/hooks/use-sku-update.ts
@@ -1,8 +1,15 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateSKU, type updateSKUPayload } from "@/lib/api/settings/sku"
 
 export function useUpdateSKU() {
+  const queryClient = useQueryClient()
+
   return useMutation({
-    mutationFn: (payload: updateSKUPayload) => updateSKU(payload)
+    mutationFn: (payload: updateSKUPayload) => updateSKU(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["inventory"],
+      })
+    },
   })
 }
